15e9770e353b788ab25cc5cd6523af0bb3a9b717
Ashish Virmani <virmani.ashish@gmail.com>
2017-08-11 20:24:11 +0530

    completed patiend CRU (delete is not needed)

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -8,13 +8,31 @@ class PatientsController < ApplicationController
     end
 
     def new
+        @patient = Patient.new
+    end
+
+    def edit
+        @patient = Patient.find(params[:id])
     end
 
     def create
         @patient = Patient.new(patient_params)
 
-        @patient.save
-        redirect_to @patient
+        if @patient.save
+            redirect_to @patient
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @patient = Patient.find(params[:id])
+
+        if @patient.update(patient_params)
+            redirect_to @patient
+        else
+            render 'edit'
+        end
     end
 
     private
