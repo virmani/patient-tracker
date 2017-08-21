0fde7898369b2ef0df35ba14236a100b7313b83c
Mayank Madan <maddiemadan@gmail.com>
2017-08-21 15:50:27 +0530

    Added Phony Rails to validate and normalize phone numbers

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -3,6 +3,10 @@ class Phone < ApplicationRecord
   
   validates :number, presence: true, length: {minimum: 9}
 
+  phony_normalize :number, default_country_code: 'IN'
+
+  validates :number, phony_plausible: true
+
   enum phone_type: [:mobile, :landline]
   enum phone_owner: [:mother, :father, :other]
 end
