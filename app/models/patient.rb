d773ed254f20cbea8088f0f27f660b90b888eb24
Ashish Virmani <virmani.ashish@gmail.com>
2019-09-05 12:38:34 -0700

    Showing last visit date on the index page instead of DOB

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,5 @@
+require 'pp'
+
 class Patient < ApplicationRecord
     default_scope { order(updated_at: :desc) }
 
@@ -13,9 +15,21 @@ class Patient < ApplicationRecord
     validates :permanent_address, presence: true, length: {minimum: 1}
     validates :permanent_district, presence: true, length: {minimum: 1}
 
+    # def last_contact
+    #     last_visit = notes.order("visit_date").first
+    #     if last_visit.present?
+    #         last_visit.visit_date
+    #     else
+    #         self.updated_at
+    #     end
+    # end
+
+    # TODO: remove the custom SQL and turn this into a scope. Some info here - https://stackoverflow.com/questions/9197649/rails-sort-by-join-table-data
     def self.all_in_updated_order
         Patient.left_outer_joins(:phones, :emails, :google_drive_folder, :notes)
-            .select('patients.id, patients.given_name, patients.last_name, patients.date_of_birth, patients.permanent_district, '\
+            .select('patients.id, patients.given_name, patients.last_name, '\
+                'COALESCE(MAX(notes.visit_date), patients.updated_at) as last_contact, '\
+                'patients.permanent_district, '\
                 'MAX('\
                     'GREATEST(patients.updated_at, emails.updated_at, google_drive_folders.updated_at, phones.updated_at, notes.updated_at)'\
                 ') AS last_updated')
